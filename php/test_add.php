e41c79d50fec4664b70c6daa25a1178ca3739c6e
raindy <raindy@free01.(none)>
2012-03-07 13:25:26 +0800

    Modify PHP test

--- php/test_add.php
+++ php/test_add.php
@@ -5,7 +5,7 @@ $squirrel = new Squirrel();
 
 $start = time();
 
-for ($i = 0; $i < 100000; $i++)
+for ($i = 0; $i < 1000000; $i++)
 	$squirrel->push_tail("-->NO number[$i] record");
 
 $end = time();
